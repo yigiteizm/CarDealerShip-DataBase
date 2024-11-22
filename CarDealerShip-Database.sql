9abb81933827e8e322e5448744ebb6d47509e033
Yigit <yigistaff@gmail.com>
2024-11-22 11:51:21 -0500

    new data base has been created.

---
+++ CarDealerShip-Database.sql
@@ -0,0 +1,17 @@
+
+-- Drop the database if it already exists
+
+DROP DATABASE IF EXISTS CarDealershipDatabase;
+
+-- Create a new database
+CREATE DATABASE CarDealershipDatabase;
+
+-- Use the newly created database
+USE CarDealershipDatabase;
+
+
+
+
+
+
+
