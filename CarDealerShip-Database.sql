4e26d2e3dda97788b9f4e94c67935832bef430dd
Yigit <yigistaff@gmail.com>
2024-11-22 11:59:06 -0500

    DROP TABLE IF EXIST has been created for related tables.

--- CarDealerShip-Database.sql
+++ CarDealerShip-Database.sql
@@ -9,6 +9,11 @@ CREATE DATABASE CarDealershipDatabase;
 -- Use the newly created database
 USE CarDealershipDatabase;
 
+-- Drop tables if they exist
+DROP TABLE IF EXISTS Dealerships;
+DROP TABLE IF EXISTS Vehicles;
+DROP TABLE IF EXISTS Inventory;
+DROP TABLE IF EXISTS Sales_contracts;
 
 
 
