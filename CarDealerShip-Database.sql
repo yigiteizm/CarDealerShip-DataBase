6f4f58edfc0fdb51e5d5dfdaca8bc45bf474d761
Yigit <yigistaff@gmail.com>
2024-11-22 13:14:31 -0500

    DealerShips list has been added to databes.

--- CarDealerShip-Database.sql
+++ CarDealerShip-Database.sql
@@ -15,6 +15,13 @@ DROP TABLE IF EXISTS Vehicles;
 DROP TABLE IF EXISTS Inventory;
 DROP TABLE IF EXISTS Sales_contracts;
 
+-- Create a table to store dealership information
+CREATE TABLE Dealerships (
+    Dealership_id INT AUTO_INCREMENT PRIMARY KEY, -- Unique, auto-incrementing ID for each dealership
+    DealershipName VARCHAR(50) NOT NULL,                    
+    Address VARCHAR(50) NOT NULL,                 
+    Phone VARCHAR(50) NOT NULL                    
+);
 
 
 
